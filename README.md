5cdfb9913ed9168d5eb530f8634792301d144d00
Monika Kiskinova <monikova@abv.bg>
2024-09-23 21:00:45 +0300

    Initial Express and Handlebars set-up

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
  JS Back-End Workshop @SoftUni
 
 ## Developing process
-1. Initial project set-up
+1. Initial project set-up;
+2. Set up Express;
+3. Set up view engine (Handlebars);
--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-console.log('Hellooooo theeeeere!');
+import express from 'express';
+import handlebars from 'express-handlebars';
+
+const app = express();
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', './src/views'); 
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('home', {layout: false}); 
+});
+
+app.listen(5000, () => console.log('Server is up and running, listening at http://localhost:5000...'));
